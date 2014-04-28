6642cf658a9bb2778c497d98fee45e68b3869668
Jon Schipp <jonschipp@gmail.com>
2014-04-27 23:59:57 -0500

    provision.sh: fix merge conflict I had

--- seconion/provision.sh
+++ seconion/provision.sh
@@ -24,3 +24,18 @@ cat <<EOF >> /etc/fstab
 # Store NSM data on other drive
 /dev/sdb	/nsm		ext4			  auto,rw,nosuid,noexec   0	  0
 EOF
+
+read -n 1 -p "Would you like me to configure multi-user GNU screen to share terminal sessions? [Y/n]" CHOICE
+
+if [ -z $CHOICE ]; then
+	CHOICE="Y"
+fi
+
+case $CHOICE in
+  y|Y) sudo apt-get -y install screen &&
+	sudo wget -O /etc/screenrc http://raw.github.com/jonschipp/misc/master/screen/screenrc &&
+	sudo chmod +s /usr/bin/screen
+  	;;
+  n|N) exit ;;
+  *) exit   ;;
+esac
