211543fbf606fe27d7f993803a71a44583582f01
Jon Schipp <jonschipp@gmail.com>
2014-05-01 00:19:26 -0500

    netsniff-ng: fixes and provision more

--- netsniff-ng/provision.sh
+++ netsniff-ng/provision.sh
@@ -6,12 +6,22 @@ HOME=/home/vagrant
 LATEST_STABLE_KERNEL_URL=http://www.kernel.org$(wget -O - https://www.kernel.org 2>/dev/null | grep -A 5 "Latest Stable Kernel" | awk -F '[""]' '/\.xz/ { print $2 }')
 KERNEL=$(echo $LATEST_STABLE_KERNEL_URL | sed 's/.*\///;s/\.tar.*$//')
 
-# Install dependencies for netsniff-ng and other things
+# Install dependencies
+
+
 sudo apt-get update
+
+sudo apt-get -y install git language-pack-en libreadline-dev
+
+# Netsniff-NG
 sudo apt-get -y install language-pack-en git ccache flex bison libnl-3-dev \
   libnl-genl-3-dev libgeoip-dev libnetfilter-conntrack-dev \
   libncurses5-dev liburcu-dev libnacl-dev libpcap-dev \
   zlib1g-dev libcli-dev libnet1-dev
+
+# Perf tools
+apt-get install libaudit-dev libunwind7-dev libelf-dev libgtk2.0-dev \
+	libnuma-dev libslang2-dev libdw-dev binutils-dev asciidoc xmlto
 
 # Compile latest Netsniff-NG
 git clone http://github.com/netsniff-ng/netsniff-ng
@@ -29,15 +39,29 @@ git clone https://github.com/netoptimizer/network-testing
 # Download, compile, and install latest stable kernel
 wget --progress=dot:mega -O - $LATEST_STABLE_KERNEL_URL | tar -xJ
 cd $KERNEL
-make defconfig
-#make allyesconfig
+#mv config $KERNEL/.config
+make config
 make -j 3
 make -j 3 modules
 sudo make modules_install
 sudo make install
 
+# Compile and install bpf debugging programs
+cd $KERNEL/tools/net
+sudo make bpf_asm
+sudo make bpf_dbg
+sudo make install
+
+# Compile and install perf tools
+#cd linux-3.12.4/tools/perf
+#make
+#make install
+#ln -s /root/bin/perf /usr/sbin/perf
+
 # System and network configuration
-sudo echo "options dummy numdummies=2" > /etc/modprobe.d/local
+sudo mv $HOME/nlmon.cfg /etc/network/interfaces.d/nlmon.cfg
+#sudo mv $HOME/dummy.cfg /etc/network/interfaces.d/dummy.cfg
+sudo sh -c 'echo "options dummy numdummies=2" > /etc/modprobe.d/local'
 cat > /etc/sysctl.d/10-bpf.conf <<EOF
 # Enable BPF JIT Compiler (approx. 50ns speed up)
 net.core.bpf_jit_enable = 2
@@ -45,14 +69,5 @@ EOF
 
 echo "Everything ran! Time for a reboot"
 sudo reboot
-
-#modprobe nlmon
-#ip link add type nlmon
-#ip link set nlmon0 up
-#Teardown:
-#      ip link set nlmon0 down
-#      ip link del dev nlmon0
-#      rmmod nlmon
-
 
 # Install Linux kernel bpf compiler, disassembler, and debugger
