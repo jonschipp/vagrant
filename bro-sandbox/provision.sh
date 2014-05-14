6cdf067362a10567020976bbe95d5eeb5f06d5c8
Jon Schipp <jonschipp@gmail.com>
2014-05-14 17:24:11 -0500

    put Docker stuff in a file and upload it instead of here-document in provision script

--- bro-sandbox/provision.sh
+++ bro-sandbox/provision.sh
@@ -4,21 +4,6 @@ sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.
 sudo apt-get update
 sudo apt-get -y install lxc-docker expect
 
-cat > Dockerfile <<EOF
-# Bro Sandbox
-#
-# VERSION               0.0.1
-FROM      ubuntu
-MAINTAINER Jon Schipp <jonschipp@gmail.com>
-RUN apt-get update
-RUN apt-get install -y build-essential cmake make gcc g++ flex bison libpcap-dev libgeoip-dev libssl-dev python-dev zlib1g-dev libmagic-dev swig2.0 wget
-RUN adduser --disabled-password --gecos "" demo
-RUN su -l -c 'wget http://www.bro.org/downloads/release/bro-2.2.tar.gz && tar -xvzf bro-2.2.tar.gz && cd bro-2.2 && ./configure && make' demo
-RUN cd /home/demo/bro-2.2 && make install
-RUN echo "PATH=$PATH:/usr/local/bro/bin/" > /etc/profile.d/bro.sh && chmod 555 /etc/profile.d/bro.sh
-RUN echo > /etc/update-motd.d/10-help-text && echo > /etc/update-motd.d/00-header
-EOF
-
 sudo docker build -t sandbox - < Dockerfile
 #sudo docker commit $(docker ps -a -q | head -n 1) sandbox
 
