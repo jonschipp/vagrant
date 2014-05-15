315e054133e12b6748b42eb2180190e526b8fd59
Jon Schipp <jonschipp@gmail.com>
2014-05-15 16:23:10 -0500

    make a lot more idempotent, remove sudo calls, not necessary

--- bro-cluster/provision-manager.sh
+++ bro-cluster/provision-manager.sh
@@ -1,49 +1,112 @@
 HOME=/home/vagrant
+ROOT=/root
+BROPATH=/usr/local/bro
 
-# Add repo for Google Perftools
-sudo add-apt-repository ppa:agent-8131/ppa
+function die {
+    echo $*
+    exit 1
+}
 
-# Install dependencies
-sudo apt-get update
-sudo apt-get -y install cmake make gcc g++ flex bison \
-	libpcap-dev libssl-dev python-dev swig zlib1g-dev libmagic-dev
+function configure_ssh {
+    # Bro uses this key to configure the nodes
+    if [ ! -d $ROOT/.ssh ]; then
+	mkdir $ROOT/.ssh
+	chown root:root $ROOT/.ssh
+	chmod 600 $ROOT/.ssh
+    fi
 
-# Install extras
-sudo apt-get -y install git libgeoip-dev gawk sendmail curl tcpreplay
-#ipsumdump (no package), libgoogle-perftools-dev
-if [ ! -f /usr/share/GeoIP/GeoIPCity.dat ]; then
-	wget --progress=dot:mega http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
-	gunzip GeoLiteCity.dat.gz
-	sudo mv GeoLiteCity.dat /usr/share/GeoIP/GeoIPCity.dat
-fi
+    if [ -f $ROOT/.ssh/id_rsa ]; then
+	mv $HOME/.ssh/id_rsa $ROOT/.ssh/id_rsa
+	chmod 400 $ROOT/.ssh/id_rsa
+    fi
+}
 
-# Install latest Bro
-if [ ! -d /usr/local/bro ]; then
-	git clone --recursive git://git.bro.org/bro
-	cd bro
-	./configure
-	make && sudo make install && make install-aux
-fi
+function configure_bro {
+    if [ ! -e /bro ]; then
+        ln -s /usr/local/bro /bro
+    fi
+    if [ ! -e /bro/site ]; then
+        (cd /bro ; sudo ln -s share/bro/site . )
+    fi
+    # Copy Bro configuration
+    if [ -e $HOME/node.cfg ]; then
+	mv $HOME/node.cfg $BROPATH/etc/node.cfg
+    fi
+    # Make Bro binaries accessible by re-setting the PATH
+    if [ ! -f /etc/profile.d/bro.sh ]; then
+	echo 'export PATH=/usr/local/bro/bin:$PATH' | sudo tee -a /etc/profile.d/bro.sh
+    fi
+}
+
+function install_dependencies {
+    # Add repo for Google Perftools
+    add-apt-repository ppa:agent-8131/ppa
+
+    # Install dependencies
+    apt-get update
+    apt-get -y install cmake make gcc g++ flex bison \
+    	libpcap-dev libssl-dev python-dev swig zlib1g-dev libmagic-dev
+}
+
+function install_extras {
+    # Install extras
+    apt-get -y install git libgeoip-dev gawk sendmail curl tcpreplay
+    #ipsumdump (no package), libgoogle-perftools-dev
+
+    if [ ! -f /usr/share/GeoIP/GeoIPCity.dat ]; then
+    	wget --progress=dot:mega http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+    	gunzip GeoLiteCity.dat.gz
+    	mv GeoLiteCity.dat /usr/share/GeoIP/GeoIPCity.dat
+    fi
+}
+
+function install_latest_bro {
+    # Install latest Bro
+    if [ ! -d /usr/local/bro ]; then
+    	git clone --recursive git://git.bro.org/bro
+    	cd bro
+    	./configure || die "Configure failed!"
+    	make || die "Build failed!"
+    	make install && make install-aux || die "Install failed!"
+    fi
+}
+
+function install_bro {
+    if [ -d /usr/local/bro ] && [ "$(bro --version 2>&1 | grep -o '[0-9]\.[0-9]')" == "$VERSION" ];
+    then
+        echo "Bro already installed"
+        return
+    fi
+    if [ ! -e bro-${VERSION}.tar.gz ] ; then
+        echo "Downloading bro"
+        wget -c http://www.bro.org/downloads/release/bro-${VERSION}.tar.gz --progress=dot:mega
+    fi
+    if [ ! -e bro-${VERSION} ] ; then
+        echo "Untarring bro"
+        tar xzf bro-${VERSION}.tar.gz
+    fi
+    cd bro-${VERSION}
+    ./configure || die "Configure failed!"
+    make || die "Build failed!"
+    make install || die "Install failed!"
+
+}
 
 cd $HOME
 
-# Make Bro binaries accessible by re-setting the PATH
-if [ ! -f /etc/profile.d/bro.sh ]; then
-	echo 'export PATH=/usr/local/bro/bin:$PATH' | sudo tee -a /etc/profile.d/bro.sh
+install_dependencies
+install_extras
+
+if [[ $1 == "latest" ]]; then
+	install_latest_bro
+elif [[ $1 =~ [0-9]\.[0-9] ]]; then
+	VERSION=$1
+	install_bro
+else
+	install_latest_bro
 fi
 
-# Bro uses this key to configure the nodes
-sudo mkdir /root/.ssh
-sudo chown root:root /root/.ssh
-sudo chmod 600 /root/.ssh
+configure_ssh
+configure_bro
 
-sudo mv ~/.ssh/id_rsa /root/.ssh/id_rsa
-sudo chmod 400 /root/.ssh/id_rsa
-
-# Copy Bro configuration
-if [ ! -f /usr/local/bro/etc/node.cfg ] && [ -f node.cfg ]; then
-	sudo mv node.cfg /usr/local/bro/etc/node.cfg
-fi
-
-# Configure
-# broctl install && broctl check && broctl start
+exit 0
