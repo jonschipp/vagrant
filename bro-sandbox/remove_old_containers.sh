35aa9db1942075f4e62aec4d9273d5bcfe468f91
Jon Schipp <jonschipp@gmail.com>
2014-08-01 17:24:35 -0500

    remove_old_containers.sh: specify days to keep
    Adds ability to specify days to keep as an argument.

--- bro-sandbox/remove_old_containers.sh
+++ bro-sandbox/remove_old_containers.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
-# Destroy containers which are older than 24 hours
-CONTAINERS=$(docker ps -a | grep -v trybro_data | grep "[0-9]\+ days ago" | awk '{ printf("%s ", $1) }')
+# Destroy containers which are older than x days (weeks, months, or years also if they were missed)
+ARGC=$#
+DAYS="$1"
+
+argcheck() {
+if [ $ARGC -lt $1 ]; then
+        echo "Please specify an argument as the max. number of days for the container to exist"
+        exit 1
+fi
+}
+
+argcheck 1
+
+CONTAINERS=$(docker ps -a | grep -v -E 'trybro_data|bro_manager' | \
+ awk -v remove=$DAYS 'BEGIN { FS="[ ]{3,}" } \
+        $4 ~ /[0-9]+ day|week|month|year/ && count=substr($4,1,2) \
+                { 
+                        if ( strtonum(count) > remove ) { 
+                                printf("%s", $1) 
+                        }
+                }')
 
 if [ ! -z "$CONTAINERS" ]
 then
