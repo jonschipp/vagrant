35290d6c13b4be7db654dcd333bb870d89b776ea
Jon Schipp <jschipp@illinois.edu>
2014-08-11 15:58:46 -0500

    provision.sh: add new lines for formatting output

--- bro-sandbox/provision.sh
+++ bro-sandbox/provision.sh
@@ -104,6 +104,7 @@ echo -e "$ORDER Installing Docker!\n"
 if [ ! -e /usr/lib/apt/methods/https ]; then
 	apt-get update
 	apt-get install -y apt-transport-https
+	echo
 fi
 
 # Add the repository to your APT sources
@@ -112,12 +113,15 @@ if [ ! -e /etc/apt/sources.list.d/docker.list ]
 then
 	echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
 	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+	echo
 fi
 
 # Install docker
 if ! command -v docker >/dev/null 2>&1
 then
-	apt-get update ; apt-get install -y lxc-docker
+	apt-get update
+	apt-get install -y lxc-docker
+	echo
 fi
 }
 
