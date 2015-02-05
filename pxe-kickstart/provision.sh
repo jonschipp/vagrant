6677409e1cb1f989d52801cd9f06c2982348d6c9
Jon Schipp <jonschipp@gmail.com>
2015-02-04 20:04:13 -0600

    pxe-kickstart/provision.sh: test network connection first

--- pxe-kickstart/provision.sh
+++ pxe-kickstart/provision.sh
@@ -146,6 +146,8 @@ start_services(){
   service apache2 restart         || die "Failed to start apache2"
 }
 
+ping -c 1 -t 2 8.8.8.8 1>/dev/null 2>/dev/null || die "Your vagrant bridge interface should be connected not to the internet!"
+
 install_dependencies "1.)"
 configuration "2.)"
 start_services "3.)"
